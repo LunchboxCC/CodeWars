05cdeef765ca882a3f4e7fcd9d19b299da2b8c29
LunchboxCC <lunchboxcc@gmail.com>
2022-06-30 11:16:16 +0200

    added test structure and solution to Bit Counting exercise

---
+++ CodeWars/CodeWars/Level/Kyu6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Level
+{
+    public static class Kyu6
+    {
+        public static int CountBits(int n)
+        {
+            /* https://www.codewars.com/kata/526571aae218b8ee490006f4
+               Write a function that takes an integer as input, and returns the number of bits that are equal to 
+               one in the binary representation of that number. You can guarantee that input is non-negative.
+
+               Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case */
+
+            var binary = Convert.ToString(n, 2);
+
+            return binary.Count(b => b == '1');
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/Kyu6Tests.cs
@@ -0,0 +1,19 @@
+﻿using CodeWars.Level;
+
+namespace CodeWarsTests
+{
+    public class Kyu6Tests
+    {
+
+        [Theory]
+        [InlineData(7, 3)]
+        [InlineData(9, 2)]
+        [InlineData(1234, 5)]
+        public void CountBits(int n, int expectedResult)
+        {
+            var result = Kyu6.CountBits(n);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
