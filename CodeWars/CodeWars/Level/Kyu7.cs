547d46352584f80d7de589a9701c2676bb6aaf4e
LunchboxCC <lunchboxcc@gmail.com>
2022-07-01 13:11:02 +0200

    You're a square! exercise

---
+++ CodeWars/CodeWars/Level/Kyu7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Level
+{
+    public static class Kyu7
+    {
+        public static bool IsSquare(int n)
+        {
+            var sqrt = Math.Sqrt(n);
+            return (int)sqrt == sqrt;
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/Kyu7Tests.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars.Level;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsTests
+{
+    public class Kyu7Tests
+    {
+        [Theory]
+        [InlineData(-1, false)]
+        [InlineData(3, false)]
+        [InlineData(4, true)]
+        [InlineData(25, true)]
+        public void IsSquare(int n, bool expectedResult)
+        {
+            var result = Kyu7.IsSquare(n);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
